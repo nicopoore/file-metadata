52cabe2154b308f0e9e39eb0aaf3cce9200a196e
Nicolás Poore <nicolaspoore@gmail.com>
2020-12-18 14:14:51 -0300

    Project completed

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const fileUpload = require('express-fileupload')
 
 var app = express();
 
@@ -11,7 +12,19 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use(fileUpload({
+  createParentPath: true
+}));
 
+app.post('/api/fileanalyse', (req, res) => {
+  console.log(req.files)
+  response = {
+    name: req.files.upfile.name,
+    type: req.files.upfile.mimetype,
+    size: req.files.upfile.size
+  }
+  res.json(response)
+})
 
 
 const port = process.env.PORT || 3000;
